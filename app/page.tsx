57185a90f7a1f82fa19b27b4cec749c82fc76e1b
raaaandom <itzraandom@gmail.com>
2026-07-29 12:43:34 +0200

    add: responsive layout wireframing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
 export default function Home() {
   return (
-    <div>
+    <div className="
+    border-2 border-red-500
+    min-h-screen flex flex-col items-center
+    ">
+      <div className="
+      border-2 border-green-500
+      min-h-screen flex flex-col justify-center gap-2
+      w-90 md:w-135 xl:w-160
+      ">
+        <div className="
+        border-2 border-blue-500 text-center
+        ">
+          Griglia
+        </div>
+        <div className="
+        border-2 border-blue-500 text-center
+        ">
+          Comandi
+        </div>
+      </div>
     </div>
   );
 }
