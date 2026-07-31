fdd61ae7ec18a9642903796e7572e797fe4fda6b
raaaandom <itzraandom@gmail.com>
2026-07-31 09:09:36 +0200

    feat: number bar (mobile input)

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,10 @@ export default function Home() {
 
   const [board, setBoard] = useState(Array(81).fill(0));
   const [selected, setSelected] = useState(-1);
+  const [picked, setPicked] = useState(-1);
 
   const gridRef = useRef<HTMLDivElement>(null);
+  const numRef = useRef<HTMLDivElement>(null);
 
   function handleClear() {
     setBoard(Array(81).fill(0));
@@ -23,20 +25,36 @@ export default function Home() {
     }
   }
 
+  function writeBoard(idx: number, value: number) {
+    const newBoard = [...board];
+    newBoard[idx] = value;
+    setBoard(newBoard);
+  }
+
   function handleCellClick(index: number) {
+    if (picked !== -1) {
+      writeBoard(index, picked)
+    };
     setSelected(index);
   }
 
-  function writeSelected(value: number) {
-    const newBoard = [...board];
-    newBoard[selected] = value;
-    setBoard(newBoard);
+  function handleNumClick(index: number) {
+      console.log(selected)
+    if (selected !== -1) {
+      writeBoard(selected, index)
+    };
+    setPicked(index);
   }
 
   useEffect(() => {
     function handleClickOutside(e: MouseEvent) {
-      if (gridRef.current && !gridRef.current.contains(e.target as Node))
+      if (!gridRef.current) return;
+      if (!numRef.current) return;
+
+      if (!gridRef.current.contains(e.target as Node) && !numRef.current.contains(e.target as Node)) {
+        setPicked(-1);
         setSelected(-1);
+      }
     }
 
     window.addEventListener("mousedown", handleClickOutside);
@@ -52,7 +70,7 @@ export default function Home() {
 
       // number input
       if (num_keys.includes(e.key)) {
-        writeSelected(Number(e.key));
+        writeBoard(selected, Number(e.key));
       }
       // escape deselection
       else if (e.key === 'Escape') {
@@ -76,7 +94,7 @@ export default function Home() {
       }
       // number deletion
       else if (e.key === 'Backspace') {
-        writeSelected(Number(0));
+        writeBoard(selected, Number(0));
       }
 
     }
@@ -96,7 +114,9 @@ export default function Home() {
         className="
         min-h-screen flex flex-col justify-center gap-6
         w-90 md:w-135 xl:w-160
+        md:text-2xl xl:text-4xl
       ">
+
         {/* Grid wrapper */}
         <div
           ref={gridRef}
@@ -115,7 +135,7 @@ export default function Home() {
                 key={index}
                 onMouseDown={() => handleCellClick(index)}
                 className={`
-                text-cell-number text-base md:text-2xl xl:text-4xl
+                text-cell-number text-base text-[length:inherit]
                 flex items-center justify-center aspect-square
                 select-none
                 border-separator-thin
@@ -131,6 +151,27 @@ export default function Home() {
             );
           })}
         </div>
+
+        {/* Numbers Panel */}
+        <div
+          ref={numRef}
+          className="
+          flex flex-row gap-1 px-1 justify-center
+          ">
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((num, idx) => {
+            return <button key={idx}
+              onClick={() => handleNumClick(idx)}
+              className={`
+              aspect-square px-2 py-1 w-full
+              border-3 border-blue-600 rounded text-blue-600
+              ${picked === idx ? "bg-blue-600 text-page-background" : ""}
+              `}
+            >
+              {num}
+            </button>
+          })}
+        </div>
+
         {/* Controls wrapper */}
         <div
           className="
@@ -164,6 +205,7 @@ export default function Home() {
             </svg>
           </button>
         </div>
+
       </div>
     </div>
   );
